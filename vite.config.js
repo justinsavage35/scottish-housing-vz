32bbfc457b80baba073d8acac0cf6f3bc0a20195
JustinS <justinsavage35@gmail.com>
2026-09-17 17:02:49 +0100

    Configure project for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/scottish-housing-vz/', // <-- Add this line (include leading and trailing slashes)
+});
